5c8aeecfa64e438a86b2b4bc547867b5f041e5ad
inglettronald <inglettronald@gmail.com>
2023-06-08 19:52:04 -0500

    Replaced openScreenDelayed with mc.send

--- src/main/kotlin/com/dulkirfabric/commands/ConfigCommand.kt
+++ src/main/kotlin/com/dulkirfabric/commands/ConfigCommand.kt
@@ -1,9 +1,7 @@
 package com.dulkirfabric.commands
 
-import com.dulkirfabric.DulkirModFabric
 import com.dulkirfabric.DulkirModFabric.mc
 import com.dulkirfabric.config.DulkirConfig
-import com.google.common.eventbus.Subscribe
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
@@ -12,8 +10,9 @@ import net.minecraft.command.CommandRegistryAccess
 object ConfigCommand {
     fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>, registryAccess: CommandRegistryAccess) {
         dispatcher.register(LiteralArgumentBuilder.literal<FabricClientCommandSource>("dulkir").executes {
-            println("command running")
-            DulkirModFabric.openScreenDelayed(DulkirConfig().screen)
+            mc.send {
+                mc.setScreen(DulkirConfig().screen)
+            }
             return@executes 0
         })
     }
