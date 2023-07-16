2003a3d2be24ae3faed0e45a8c6cc5e0b8343cc3
inglettronald <inglettronald@gmail.com>
2023-07-16 10:24:01 -0500

    Remove Scoreboard Numbers.

--- src/main/java/com/dulkirfabric/mixin/render/InGameHudMixin.java
+++ src/main/java/com/dulkirfabric/mixin/render/InGameHudMixin.java
@@ -1,11 +1,14 @@
 package com.dulkirfabric.mixin.render;
 
 import com.dulkirfabric.config.DulkirConfig;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.scoreboard.ScoreboardObjective;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
@@ -16,5 +19,11 @@ public class InGameHudMixin {
         if (DulkirConfig.ConfigVars.getConfigOptions().getStatusEffectHidden()) {
             ci.cancel();
         }
+    }
+
+    @Redirect(method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/scoreboard/ScoreboardObjective;)V",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;IIIZ)I"))
+    public int renderScoreBoardSidebar(DrawContext instance, TextRenderer textRenderer, String text, int x, int y, int color, boolean shadow) {
+        return 0;
     }
 }
