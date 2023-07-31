38ab76c017ab0bd6ff1dd1f8e9b707662c785a53
ronald <inglettronald@gmail.com>
2023-07-31 12:24:12 -0500

    Fixed RenderTest error

--- src/main/kotlin/com/dulkirfabric/features/RenderTest.kt
+++ src/main/kotlin/com/dulkirfabric/features/RenderTest.kt
@@ -12,19 +12,19 @@ import java.awt.Color
 
 object RenderTest {
 
-    @EventHandler
-    fun onRender(event: WorldRenderLastEvent) {
-        WorldRenderUtils.renderWaypoint(
-            Text.literal("Home Base").setStyle(Style.EMPTY.withColor(Color(255, 100, 150, 255).rgb)), event.context,
-            Vec3d(-183.5, 79.0, -465.5)
-        )
-
-        WorldRenderUtils.drawBox(event.context, 16.0, 119.0, -6.0, 2.0, 2.0, 2.0, Color(0, 200, 200, 100), false)
-    }
-
-    @EventHandler
-    fun onLoadEnt(event: EntityLoadEvent) {
-        //if (event.entity !is GlowingEntityInterface) return
-        //event.entity.setDulkirEntityGlow(true, Color(0, 0, 255, 255))
-    }
+//    @EventHandler
+//    fun onRender(event: WorldRenderLastEvent) {
+//        WorldRenderUtils.renderWaypoint(
+//            Text.literal("Home Base").setStyle(Style.EMPTY.withColor(Color(255, 100, 150, 255).rgb)), event.context,
+//            Vec3d(-183.5, 79.0, -465.5)
+//        )
+//
+//        WorldRenderUtils.drawBox(event.context, 16.0, 119.0, -6.0, 2.0, 2.0, 2.0, Color(0, 200, 200, 100), false)
+//    }
+//
+//    @EventHandler
+//    fun onLoadEnt(event: EntityLoadEvent) {
+//        //if (event.entity !is GlowingEntityInterface) return
+//        //event.entity.setDulkirEntityGlow(true, Color(0, 0, 255, 255))
+//    }
 }
