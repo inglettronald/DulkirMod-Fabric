394ccbdeea495381c2ea4cdf576cf13287caf9f1
Inglett <inglettronald@gmail.com>
2024-07-14 12:45:14 -0500

    1.21: fix inventory scaling player render and github workflow

---
+++ src/main/java/com/dulkirfabric/mixin/render/InventoryScreenMixin.java
@@ -0,0 +1,25 @@
+package com.dulkirfabric.mixin.render;
+
+import com.dulkirfabric.config.DulkirConfig;
+import com.dulkirfabric.features.InventoryScale;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(InventoryScreen.class)
+public class InventoryScreenMixin {
+    @ModifyArgs(
+            method = "drawEntity(Lnet/minecraft/client/gui/DrawContext;IIIIIFFFLnet/minecraft/entity/LivingEntity;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/DrawContext;enableScissor(IIII)V"
+            )
+    )
+    private static void modifyScissor(Args args) {
+        for (int i = 0; i < 4; i++) {
+            args.set(i, (int) (((int) args.get(i)) * InventoryScale.INSTANCE.getScale()));
+        }
+    }
+}
