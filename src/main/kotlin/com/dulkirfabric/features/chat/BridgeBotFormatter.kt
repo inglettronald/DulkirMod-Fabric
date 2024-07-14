090f3f6aacc1e4f5d6bfd05b98dd42dbc328f2e2
Inglett <inglettronald@gmail.com>
2024-07-14 13:10:43 -0500

    1.21: fix bridge bot issue

--- src/main/kotlin/com/dulkirfabric/features/chat/BridgeBotFormatter.kt
+++ src/main/kotlin/com/dulkirfabric/features/chat/BridgeBotFormatter.kt
@@ -11,7 +11,7 @@ object BridgeBotFormatter {
     private val bridgeRegex: Regex = "^(§2Guild|§3Officer) > (?:\\S+ )?([\\w§]{3,18})(?: §[a-z0-9][[A-Z]+])?§f: ([^>]+)(?: >| »|:) (.+)".toRegex()
 
     @EventHandler
-    fun onChat(event: ChatEvents.AllowChat) {
+    fun onChat(event: ChatEvents.ModifyChat) {
         if (!DulkirConfig.configOptions.bridgeFormatter) return
 
         val message = event.message.string
@@ -20,13 +20,14 @@ object BridgeBotFormatter {
             val (prefix, name, playerName) = matchResult!!.destructured
             if (TextUtils.stripColorCodes(name).equals(DulkirConfig.configOptions.bridgeBotName, ignoreCase = true)) {
                 val newPrefix = if (prefix == "§2Guild") "§2Bridge" else "§3Bridge"
-                event.message.siblings[0] = Text.literal(
-                    "$newPrefix > "
-                ).append(Text.literal(playerName).setStyle(Style.EMPTY.withColor(DulkirConfig.configOptions.bridgeNameColor)))
-                    .append(": ")
-                event.message.siblings[1] = Text.literal(
-                    event.message.siblings[1].string.replace("$playerName > ", "")
-                ).setStyle(event.message.siblings[1].style)
+                val newMessage = event.message.copy()
+                newMessage.siblings[0] = Text.literal("$newPrefix > ")
+                        .append(Text.literal(playerName).setStyle(Style.EMPTY.withColor(DulkirConfig.configOptions.bridgeNameColor)))
+                        .append(": ")
+                newMessage.siblings[1] = Text.literal(event.message.siblings[1].string.replace("$playerName > ", ""))
+                        .setStyle(event.message.siblings[1].style)
+
+                event.setReturnValue(newMessage)
             }
         }
     }
